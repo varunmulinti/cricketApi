386c1f304582ed85d28724711caa626cb8f1431f
varunmulinti <varun9.run@gmail.com>
2016-05-17 12:22:03 +0530

    created first page

---
+++ app.js
@@ -0,0 +1,19 @@
+var cricketApp = angular.module('cricApi', ['ngResource','ngRoute']);
+
+angularApp.config(function ($routeProvider){
+  $routeProvider
+  .when ('/',{
+    templateUrl: 'pages/main.html',
+    controller: 'HomeController'
+  })
+});
+angularApp.controller("HomeController",function($resource){
+  var vm=this;
+  vm.getCityTemperature = function(){
+    console.log(vm.cityName);
+    var weatherResource = $resource('http://api.openweathermap.org/data/2.5/weather',
+      {q:vm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa'});
+    vm.weatherResponse = weatherResource.get();
+    console.log(vm.weatherResponse);
+   }
+});
