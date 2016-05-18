3838ee49a4874e48ed94981d7268a825cbad4f0d
varunmulinti <varun9.run@gmail.com>
2016-05-18 21:41:34 +0530

    left with UI

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ angularApp.config(function ($routeProvider){
   $routeProvider
   .when ('/',{
     templateUrl: 'pages/main.html',
-    controller: 'HomeController'
+    controller: 'HomeController',
+    controllerAs:'hm'
   })
-
+  .when ('/cricketdetails/:uniqId',{
+    templateUrl: 'pages/details.html',
+    controller: 'DetailsController',
+    controllerAs:'dc'
+  })
 });
 
 
@@ -16,3 +21,11 @@ angularApp.controller("HomeController",['$resource',function($resource){
     vm.cricResponse = cricResource.get();
     console.log(vm.cricResponse);
 }]);
+
+angularApp.controller("DetailsController",['$resource','$routeParams',function($resource,$routeParams){
+  var vm=this;
+  var id = $routeParams.uniqId;
+  var cricResource = $resource('http://cricapi.com/api/cricketScore',{unique_id:id});
+  vm.cricResponse = cricResource.get();
+  console.log(vm.cricResponse);
+}])
